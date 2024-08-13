73e20e57c986e714041dcd7926a796efa869f7b8
UNDER <53385176+UNDER192103@users.noreply.github.com>
2024-08-13 03:31:53 -0300

    Add files via upload

--- Repository/DB.js
+++ Repository/DB.js
@@ -3,9 +3,7 @@ const fs = require("fs");
 const _MAIN_DIR = __dirname.split('Repository')[0];
 const packageJson = require(_MAIN_DIR+"/package.json");
 const Jsoning = require("jsoning");
-
-var dir_appdata_un = path.join(process.env.APPDATA, packageJson.name);
-var dir_appdata_un_data = path.join(dir_appdata_un, 'UN-DATA');
+const dir_appdata_un_data = path.join(path.join(process.env.APPDATA, packageJson.name), 'UN-DATA');
 
 var _all  = require(_MAIN_DIR + 'Domain/Model/DB_model.js');
 
@@ -13,7 +11,7 @@ _all.DB = new Jsoning(dir_appdata_un_data + "/DB/DB.json");
 _all.Opens_windows = new Jsoning(dir_appdata_un_data + "/DB/Opens_windows.json");
 _all.List_macros = new Jsoning(dir_appdata_un_data + "/DB/Macros.json");
 _all.ProgramsExe = new Jsoning(dir_appdata_un_data + "/DB/ProgramsExe.json");
-_all.DB_DIR = dir_appdata_un;
+_all.DB_DIR = path.join(process.env.APPDATA, packageJson.name);
 
 _all.GetDataNow = async ()=>{
     _all.DB = new Jsoning(dir_appdata_un_data + "/DB/DB.json");
